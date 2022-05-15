9b5565d40379f6e81f7b958e91a51208416a0c4b
Fabian <acostahf4@gmail.com>
2022-05-15 10:12:59 -0500

    smart contract

--- contracts/mugs.sol
+++ contracts/mugs.sol
@@ -6,6 +6,16 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Mugs is ERC721, Pausable, Ownable {
+    bool public saleIsActive = false;
+    string private _baseURIextended;
+
+    bool public isAllowListActive = false;
+    uint256 public constant MAX_SUPPLY = 1000;
+    uint256 public constant MAX_PUBLIC_MINT = 5; 
+    uint256 public constant PRICE_PER_TOKEN = 0.1 ether;
+
+    mapping(address => uint8) private _allowList;
+
     constructor() ERC721("MUGS", "MUG") {}
 
     function pause() public onlyOwner {
