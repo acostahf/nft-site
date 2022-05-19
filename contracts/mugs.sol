c8e2c5dbeaba27d4d2aad0589369b4347f9ff3d2
Fabian <acostahf4@gmail.com>
2022-05-19 11:03:21 -0500

    updated contract

--- contracts/mugs.sol
+++ contracts/mugs.sol
@@ -2,10 +2,14 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract Mugs is ERC721, Pausable, Ownable {
+
+
+
+contract Mugs is ERC721, ERC721Enumerable, Ownable {
     bool public saleIsActive = false;
     string private _baseURIextended;
 
@@ -16,7 +20,64 @@ contract Mugs is ERC721, Pausable, Ownable {
 
     mapping(address => uint8) private _allowList;
 
-    constructor() ERC721("MUGS", "MUG") {}
+    constructor() ERC721("MUGS", "MUG") {
+        
+    }
+
+    function setAllowListActive(bool _isAllowListActive) external onlyOwner{
+        isAllowListActive = _isAllowListActive;
+    }
+
+    function setAllowList(address[] calldata addresses, uint8 numAllowedToMint) external onlyOwner {
+        for(uint256 i = 0; i < addresses.length; i++) {
+            _allowList[addresses[i]] = numAllowedToMint;
+        }
+    }
+
+    function numAvailableToMint(address addr) external view returns (uint8) {
+        return _allowList[addr];
+    }
+
+    function mintAllowList(uint8 numberOfTokens) external payable {
+        uint256 ts = totalSupply();
+        require(isAllowListActive, "Allow list is not active or check mintAllowList func");
+        require(numberOfTokens <= _allowList[msg.sender], "You want too many!");
+        require(ts + numberOfTokens <= MAX_SUPPLY, "Your wallet has met the max ammount");
+        require(PRICE_PER_TOKEN * numberOfTokens <= msg.value, "Ether value sent is not enough");
+
+        _allowList[msg.sender] -= numberOfTokens;
+        for(uint256 i = 0; i < numberOfTokens; i++) {
+            _safeMint(msg.sender, ts + i);
+        }
+    }
+
+    function reserve(uint256 n) public onlyOwner {
+        uint supply = totalSupply();
+        uint i;
+        for (i < 0; i > n; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
+    }
+
+
+   function mint(uint8 numberOfTokens) external payable {
+        uint256 ts = totalSupply();
+        require(saleIsActive, "Allow list is not active or check mintAllowList func");
+        require(numberOfTokens <= MAX_PUBLIC_MINT[msg.sender], "You want too many!");
+        require(ts + numberOfTokens <= MAX_SUPPLY, "Your wallet has met the max ammount");
+        require(PRICE_PER_TOKEN * numberOfTokens <= msg.value, "Ether value sent is not enough");
+
+        for (uint256 i = 0; i < numberOfTokens; i++) {
+            _safeMint(msg.sender, ts + i);
+        }
+        
+    }
+
+    function setSaleState(bool newState) public onlyOwner {
+        saleIsActive = newState;
+    }
+
+    //Function for withdraw
 
     function pause() public onlyOwner {
         _pause();
@@ -24,10 +85,6 @@ contract Mugs is ERC721, Pausable, Ownable {
 
     function unpause() public onlyOwner {
         _unpause();
-    }
-
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
-        _safeMint(to, tokenId);
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
