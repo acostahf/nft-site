48d1e49d101bc3b7059dad0f32f48c1933822b7d
ChristianStaubo <christianlewisstop@gmail.com>
2022-05-21 11:16:49 -0500

    imported navbar from react sandbox

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,63 +1,72 @@
 import React, { useState } from 'react'
-import {MenuIcon} from '@heroicons/react/outline'
+import Link from 'next/link'
 function Navbar() {
   const [hamburgerOpen, setHamburgerOpen] = useState(false)
 
   const handleClick = () => {
     setHamburgerOpen(!hamburgerOpen)
+    console.log('Clicked')
   }
   return (
-    <div className=' h-20 w-screen fixed'>
-        <div className=' h-full w-full justify-between items-center px-2 flex '>
-          <div className='flex items-center'>
-
+    <div className='relative container mx-auto p-6'>
+        {/* FLEX CONTAINER */}
+        <div className='flex justify-between items-center'>
             {/* LOGO */}
-            <h1 className='text-3xl lg:text-4xl mx-4 font-bold'>NFT </h1>
-          </div>
-            {/* MIDDLE MENU ITEMS */}
-            <div className=' mx-12 hidden lg:flex'>
-              <ul className='hidden lg:flex '>
-                <li className='navBarLi'>Home</li>
-                <li className='navBarLi'>About</li>
-                <li className='navBarLi'>Roadmap</li>
-                <li className='navBarLi'>Showcase</li>
-                <li className='navBarLi'>Team</li>
-              </ul>
+            <div className='pt-2'>
+                <h1 className='logo text-2xl font-bold text-indigo-500'>NFT</h1>
             </div>
-
+            {/* MENU */}
+            <div className='hidden md:flex space-x-6'>
+              <Link href="/home">
+                <a  className='hover:text-slate-400 font-semibold'>Home</a>
+                </Link>
+                <Link href="/home">
+                <a  className='hover:text-slate-400 font-semibold'>About</a>
+                </Link>
+                <Link href="/home">
+                <a  className='hover:text-slate-400 font-semibold'>Roadmap</a>
+                </Link>
+                <Link href="/home">
+                <a  className='hover:text-slate-400 font-semibold'>Showcase</a>
+                </Link>
+                <Link href="/home">
+                <a  className='hover:text-slate-400 font-semibold'>Team</a>
+                </Link>
+            </div>
+            {/* BUTTON */}
+            <Link href="/home">
+              <a  className='swipeBtn overflow-hidden hidden md:block p-3 px-6 pt-2 rounded-full border-2 border-black '>Connect wallet</a>
+            </Link>
             
-          {/* HAMBURGER MENU ICON   */}
-          
-          <div className='lg:hidden' onClick={handleClick}>
-            {!hamburgerOpen ? <MenuIcon className='w-5' /> : <p>down icon</p> }
-              
-            </div>
-          <div className='hidden lg:flex '>
-              <button className='bg-indigo-600 text-white rounded-lg px-8 py-3'>Connect wallet</button>
-            </div>
-          <div className='lg:hidden rounded-full bg-emerald-400'>
-            <p>Circle</p>
 
-          </div>
-         
+            {/* HAMBURGER MENU ICON */}
+            <button onClick={handleClick} className= {!hamburgerOpen ? 'block hamburger md:hidden focus:outline-none' : 'block open hamburger md:hidden focus:outline-none'}>
+              <span className='hamburger-top'></span>
+              <span className='hamburger-middle'></span>
+              <span className='hamburger-bottom'></span>
+            </button>
 
         </div>
 
-        {/* DROPDOWN MENU */}
+        {/* MOBILE MENU */}
+        <div className='md:hidden'>
+          <div className={!hamburgerOpen ? 'hidden' : 'flex-col absolute h-screen w-full items-center self-end flex py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 z-10 drop-shadow-md'}>
+            <Link href="/home">
+              <a >Home</a>
+            </Link>
+            
+            <Link href="/home">
+              <a >About</a>
+            </Link>
+            <Link href="/home">
+              <a >Showcase</a>
+            </Link>
+            <Link href="/home">
+              <a >Roadmap</a>
+            </Link>
+          </div>
 
-        <ul className={!hamburgerOpen ? 'hidden' : 'absolute flex flex-col items-center bg-slate-700 w-full px-8 '}>
-                <li className='navBarLi'>Home</li>
-                <li className='navBarLi'>About</li>
-                <li className='navBarLi'>Roadmap</li>
-                <li className='navBarLi'>Showcase</li>
-                <li className='navBarLi'>Team</li>
-                <div className='flex flex-col'>
-                  <button className='bg-indigo-600 text-white rounded-lg px-8 py-3'>Connect wallet</button>
-                </div>
-                
-        </ul>
-        
-
+        </div>
     </div>
   )
 }
