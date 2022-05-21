cc065fc0bc18082b3be98f0b8e1dfc348f3ff027
ChristianStaubo <christianlewisstop@gmail.com>
2022-05-21 11:33:55 -0500

    added team section from react sandbox

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Head from "next/head";
 import Image from "next/image";
 import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
+import Team from "../components/Team";
 import reactPng from './assets/logo192.png'
 export default function Home() {
 	return (
 		<div >
         	<Hero />
+			<Team />
 		</div>
 	);
 }
