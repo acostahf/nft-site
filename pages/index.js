89a14302cb285c2cc0c7f3415d4635e4d6e2b920
ChristianStaubo <christianlewisstop@gmail.com>
2022-05-21 11:35:37 -0500

    added timeline section from react sandbox

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Image from "next/image";
 import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
 import Team from "../components/Team";
+import Timeline from "../components/Timeline";
 import reactPng from './assets/logo192.png'
 export default function Home() {
 	return (
 		<div >
         	<Hero />
 			<Team />
+			<Timeline />
 		</div>
 	);
 }
