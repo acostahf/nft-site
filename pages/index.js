e4ef5c35f84f2be27a1ee158336ea6fe63c4d15c
ChristianStaubo <christianlewisstop@gmail.com>
2022-05-21 11:29:01 -0500

    added hero page from react sandbox

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
+import Hero from '../components/Hero'
+import Navbar from '../components/Navbar'
+import reactPng from './assets/logo192.png'
 export default function Home() {
 	return (
-		<div className={styles.container}>
-			
+		<div >
+        	<Hero />
 		</div>
 	);
 }
---
+++ pages/landingPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Navbar from '../components/Navbar'
+
+function landingPage() {
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+        {/* <Team />
+        <Timeline />
+        <Footer /> */}
+    </div>
+  )
+}
+
+export default landingPage
