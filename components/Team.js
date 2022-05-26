7d6e581204091f458aae9d4cee4b8b6f61590ee6
ChristianStaubo <christianlewisstop@gmail.com>
2022-05-26 13:57:25 -0500

    added social icons to team cards

--- components/Team.js
+++ components/Team.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import reactPng from './assets/logo192.png'
 import Image from 'next/image'
+import { BeakerIcon } from '@heroicons/react/solid'
 function Team() {
   return (
       <>
        {/* HEADING */}
       <h2 className='text-4xl md:text-6xl font-bold text-center'>Meet the team</h2>
      
-    <div className='max-w-6x; px-5 mx-auto pt-20  text-center md:h-screen h-[1500px] teamDiv flex justify-evenly'>
+    <div className='max-w-6x; px-5 mx-auto pt-20  text-center md:h-screen h-[1500px] teamDiv flex justify-evenly pb-20'>
        
         
             {/* TEAM MEMBER 1 */}
@@ -19,7 +20,13 @@ function Team() {
                 {/* <img src={reactPng} className='w-16 z-10 mx-auto relative top-[-50px]' alt='teamMember1' /> */}
                     <div className='card-content'>
                         <h2 className='card-title'>Fabian, CEO</h2>
-                            <p className='card-body pb-8'>Socials could go here with icons/buttons</p>
+                            <p className='card-body pb-8 '>Contact through one of these social medias</p>
+                            <div className='flex flex-row justify-around'>
+                                <BeakerIcon className="h-5 w-5 text-blue-500"/>
+                                <BeakerIcon className="h-5 w-5 text-blue-500"/>
+                                <BeakerIcon className="h-5 w-5 text-blue-500"/>
+                            </div>
+                            
                     </div>
                     </div>
                 </div>
@@ -34,7 +41,12 @@ function Team() {
                 {/* <img src={reactPng} className='w-16 z-10 mx-auto relative top-[-50px]' alt='teamMember1' /> */}
                     <div className='card-content'>
                     <h2 className='card-title'>Daena, CFO</h2>
-                            <p className='card-body pb-8'>Socials could go here with icons/buttons</p>
+                    <p className='card-body pb-8 '>Contact through one of these social medias</p>
+                            <div className='flex flex-row justify-around'>
+                                <BeakerIcon className="h-5 w-5 text-blue-500"/>
+                                <BeakerIcon className="h-5 w-5 text-blue-500"/>
+                                <BeakerIcon className="h-5 w-5 text-blue-500"/>
+                            </div>
                     </div>
                     </div>
                 </div>
@@ -53,7 +65,12 @@ function Team() {
                     <div className='card-content'>
                     
                     <h2 className='card-title'>Christian, C3PO</h2>
-                            <p className='card-body pb-8'>Socials could go here with icons/buttons</p>
+                    <p className='card-body pb-8 '>Contact through one of these social medias</p>
+                            <div className='flex flex-row justify-around'>
+                                <BeakerIcon className="h-5 w-5 text-blue-500"/>
+                                <BeakerIcon className="h-5 w-5 text-blue-500"/>
+                                <BeakerIcon className="h-5 w-5 text-blue-500"/>
+                            </div>
                     </div>
                     </div>
                 </div>
@@ -68,7 +85,12 @@ function Team() {
                 {/* <img src={reactPng} className='w-16 z-10 mx-auto relative top-[-50px]' alt='teamMember1' /> */}
                     <div className='card-content'>
                     <h2 className='card-title'>Artist, CAIO</h2>
-                            <p className='card-body pb-8'>Socials could go here with icons/buttons</p>
+                    <p className='card-body pb-8 '>Contact through one of these social medias</p>
+                            <div className='flex flex-row justify-around'>
+                                <BeakerIcon className="h-5 w-5 text-blue-500"/>
+                                <BeakerIcon className="h-5 w-5 text-blue-500"/>
+                                <BeakerIcon className="h-5 w-5 text-blue-500"/>
+                            </div>
                     </div>
                     </div>
                 </div>
